61538fefe9cad812a720d0d972ce062bd5441267
ilnurzif <ilnurzif@gmail.com>
2020-01-14 13:50:37 +0300

    stable

---
+++ Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+    @FXML
+    TextArea mainArea;
+
+    @FXML
+    TextField msgField;
+
+
+    public void sendMsgAction(ActionEvent actionEvent) {
+        if (msgField.getText().trim().length() > 0) {
+            mainArea.appendText(msgField.getText() + "\n");
+            msgField.clear();
+            msgField.requestFocus();
+        }
+    }
+}
